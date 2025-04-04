f70daebf033baa8e0c462b4e093d288f78dc92fb
Nguyen Vy Phuong Bui <buinguyenvyphuong@gmail.com>
2025-04-04 13:41:10 +1000

    Custom Dropdown

--- selenium/webdriver/Topic_10_Custom_Dropdown.java
+++ selenium/webdriver/Topic_10_Custom_Dropdown.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -33,8 +34,45 @@ public class Topic_10_Custom_Dropdown {
         selectItemInDropdown("span#speed-button","ul#speed-menu div", "Fast");
     }
 
+    @Test
+    public void TC_02_React_Semantic() throws InterruptedException {
+        driver.get("https://react.semantic-ui.com/maximize/dropdown-example-selection/");
+        selectItemInDropdown("div.dropdown","div.item span.text","Elliot Fu");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Elliot Fu");
+
+        selectItemInDropdown("div.dropdown","div.item span.text","Christian");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.divider.text")).getText(), "Christian");
+    }
+
+    @Test
+    public void TC_03_VueJS() throws InterruptedException {
+        driver.get("https://mikerodham.github.io/vue-dropdowns/");
+        selectItemInDropdown("li.dropdown-toggle","ul.dropdown-menu  a","First Option");
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), "First Option");
+
+        selectItemInDropdown("li.dropdown-toggle","ul.dropdown-menu  a","Second Option");
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), "Second Option");
+
+        selectItemInDropdown("li.dropdown-toggle","ul.dropdown-menu  a","Third Option");
+        Assert.assertEquals(driver.findElement(By.cssSelector("li.dropdown-toggle")).getText(), "Third Option");
+    }
+
+    @Test
+    public void TC_04_Editable() throws InterruptedException {
+        driver.get("https://id5.cloud.huawei.com/CAS/portal/userRegister/regbyemail.html");
+
+        selectItemInHuaweiDropdown("div[ht='input_emailregister_dropdown']","input[ht='input_emailregister_search']"
+                ,"ul.hwid-alpla-list span", "Vietnam");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[ht='input_emailregister_dropdown")).getText(), "Vietnam");
+
+        selectItemInHuaweiDropdown("div[ht='input_emailregister_dropdown']","input[ht='input_emailregister_search']"
+                ,"ul.hwid-alpla-list span", "Australia");
+        Assert.assertEquals(driver.findElement(By.cssSelector("div[ht='input_emailregister_dropdown")).getText(), "Australia");
+
+    }
+
+
     private void selectItemInDropdown(String parentLocator, String childLocator, String textItem) throws InterruptedException {
-        //driver.findElement(By.cssSelector()).click();
         driver.findElement(By.cssSelector(parentLocator)).click();
         Thread.sleep(2000);
 
@@ -44,7 +82,6 @@ public class Topic_10_Custom_Dropdown {
 
         //Save them to a list
         List<WebElement> allItems = driver.findElements(By.cssSelector(childLocator));
-        //List<WebElement> allItems = driver.findElements(By.cssSelector("ul#speed-menu div"));
 
         //Check each item
         for (WebElement item : allItems){
@@ -55,10 +92,52 @@ public class Topic_10_Custom_Dropdown {
         }
     }
 
-    @Test
-    public void TC_02_() {
+    private void selectItemInEditableDropdown(String parentLocator, String childLocator, String textItem) throws InterruptedException {
+        driver.findElement(By.cssSelector(parentLocator)).clear();
+        driver.findElement(By.cssSelector(parentLocator)).sendKeys(textItem);
+        Thread.sleep(2000);
 
+        //Wait till all items get loaded
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.
+                presenceOfAllElementsLocatedBy(By.cssSelector(childLocator)));
+
+        //Save them to a list
+        List<WebElement> allItems = driver.findElements(By.cssSelector(childLocator));
+
+        //Check each item
+        for (WebElement item : allItems){
+            if (item.getText().equals(textItem)){
+                item.click();
+                break;
+            }
+        }
     }
+
+    private void selectItemInHuaweiDropdown(String parentLocator, String editableLocator, String childLocator, String textItem) throws InterruptedException {
+        new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.elementToBeClickable(By.cssSelector(parentLocator)));
+        driver.findElement(By.cssSelector(parentLocator)).click();
+        Thread.sleep(1000);
+
+        driver.findElement(By.cssSelector(editableLocator)).clear();
+        driver.findElement(By.cssSelector(editableLocator)).sendKeys(textItem);
+        Thread.sleep(2000);
+
+        //Wait till all items get loaded
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.
+                presenceOfAllElementsLocatedBy(By.cssSelector(childLocator)));
+
+        //Save them to a list
+        List<WebElement> allItems = driver.findElements(By.cssSelector(childLocator));
+
+        //Check each item
+        for (WebElement item : allItems){
+            if (item.getText().equals(textItem)){
+                item.click();
+                break;
+            }
+        }
+    }
+
 
     @AfterClass
     public void afterClass(){
